721d301a9b64e100a3c8c94180bee30c0cd9a4b8
胡彬 <hzhubin15@corp.netease.com>
2016-08-10 11:48:51 +0800

    misc:Spring Cloud Bus 成功

--- config-client/src/main/java/config/ApplicationClient.java
+++ config-client/src/main/java/config/ApplicationClient.java
@@ -3,11 +3,13 @@ package config;
 import org.hibernate.validator.internal.constraintvalidators.hv.ParameterScriptAssertValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * Created by hzhubin15 on 2016/8/10.
  */
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ApplicationClient {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationClient.class,args);
--- config-client/src/main/java/config/LuckyWordController.java
+++ config-client/src/main/java/config/LuckyWordController.java
@@ -2,17 +2,27 @@ package config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ConfigurationProperties(prefix = "wordConfig")
+//@ConfigurationProperties(prefix = "wordConfig")
+@RefreshScope
 public class LuckyWordController {
 
-	@Value("${lucky-word}") String luckyWord;
-
+//	String luckyWord;
+	@Value("${wordConfig.luckyWord}")String luckyWord;
 	@RequestMapping("/lucky-word")
 	public String showLuckyWord() {
 		return "The lucky word is: " + luckyWord;
 	}
+
+//	public String getLuckyWord() {
+//		return luckyWord;
+//	}
+//
+//	public void setLuckyWord(String luckyWord) {
+//		this.luckyWord = luckyWord;
+//	}
 }
